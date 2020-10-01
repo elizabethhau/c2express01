1b5120176f09216888ff7e1643c007b88c073f9e
Elizabeth Hau <elizabeth.h.hau@gmail.com>
2020-10-01 15:49:10 -0400

    add tests and yml file

--- contacts.js
+++ contacts.js
@@ -4,40 +4,54 @@ const fetch = require("node-fetch");
 app.use(express.json());
 // store contacts in an arrays
 var contacts = [
-  {
-    name: "peter parker",
-    age: 21,
-    email: "peter@mit.edu",
-    courses: [
-      { number: "1.00", name: "engr comp" },
-      { number: "3.00", name: "intro bio" },
-    ],
-  },
-  {
-    name: "bruce wayne",
-    age: 32,
-    email: "bruce@mit.edu",
-    courses: [
-      { number: "2.00", name: "intro ME" },
-      { number: "3.00", name: "intro MS" },
-    ],
-  },
-  {
-    name: "diana prince",
-    age: 25,
-    email: "diana@mit.edu",
-    courses: [
-      { number: "2.00", name: "intro arch" },
-      { number: "1.00", name: "intro chem" },
-    ],
-  },
+  // {
+  //   name: "peter parker",
+  //   age: 21,
+  //   email: "peter@mit.edu",
+  //   courses: [
+  //     { number: "1.00", name: "engr comp" },
+  //     { number: "3.00", name: "intro bio" },
+  //   ],
+  // },
+  // {
+  //   name: "bruce wayne",
+  //   age: 32,
+  //   email: "bruce@mit.edu",
+  //   courses: [
+  //     { number: "2.00", name: "intro ME" },
+  //     { number: "3.00", name: "intro MS" },
+  //   ],
+  // },
+  // {
+  //   name: "diana prince",
+  //   age: 25,
+  //   email: "diana@mit.edu",
+  //   courses: [
+  //     { number: "2.00", name: "intro arch" },
+  //     { number: "1.00", name: "intro chem" },
+  //   ],
+  // },
 ];
 
+const getData = async () => {
+  let url = "https://pollysnips.s3.amazonaws.com/users.json";
+  try {
+    let res = await fetch(url)
+    contacts = await res.json();
+    console.log('success')
+    console.log(contacts);
+  } catch (error) {
+    console.log("error");
+  }
+ };
+//  getData()
+ 
 app.get("/", function (req, res) {
   res.send("<h1> Hello Routes: try POST to /contact and GET /contacts </h1>");
 });
 // list all contacts
 app.get("/contacts", function (req, res) {
+  // getData()
   res.json(contacts);
 });
 // add a contact
